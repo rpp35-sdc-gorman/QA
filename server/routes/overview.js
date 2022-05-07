541b13b1699058fcf78106f7c70975c5406d6d74
Digitell-Chris-S <95701436+Digitell-Chris-S@users.noreply.github.com>
2022-05-07 14:49:51 -0400

    Implemented sendRequest method

--- server/routes/overview.js
+++ server/routes/overview.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+const sendRequest = require('../lib/sendRequest');
 
 router.get('/', (req, res, next) => {
-
-
-
+  const path = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp35/products'
+  sendRequest(path)
+  .then(data => {
+    console.log(data)
+    res.json(data.data)
+  })
+  .catch(err => {
+    next(err)
+  })
 });
 
 
