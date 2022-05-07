424627411602413c94d94354836525fb1125b940
Kevin On <idealistkevin@gmail.com>
2022-05-07 14:49:02 -0700

    Refactor to get related items instead of just product

--- server/API/RIC.js
+++ server/API/RIC.js
@@ -3,7 +3,6 @@ const axios = require('axios');
 
 const getRICProducts = (endpoint) => {
   return axios.get(`${config.HOST + endpoint}`, {
-    params: {},
     headers: {
       'Authorization': config.TOKEN,
       'Content-Type': 'application/json'
--- server/routes/relatedItemsAndComparisons.js
+++ server/routes/relatedItemsAndComparisons.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const getRICProducts = require('../API/RIC.js');
 
 router.get('/', (req, res, next) => {
-  const endpoint = 'products';
+  // will need to refactor based on if we are using query or parameters
+  const productId = req.query.product_id;
+  const endpoint = `products/${productId}/related`;
   getRICProducts(endpoint)
     .then(response => {
       res.json(response.data);
