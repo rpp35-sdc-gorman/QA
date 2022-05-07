d00e0a9a776aa0379bfb503613d68ae739a561c6
Ian Campbell <ian.campbell.92@gmail.com>
2022-05-07 14:25:26 -0600

    added ratingReview to main div

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
+import RatingsAndReviews from './components/ratingsAndReviews/ratingsAndReviews.jsx';
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {};
   }
 
   render() {
-    return(
+    return (
       <div>
         <h1>dsfjkahsdl</h1>
+        <RatingsAndReviews></RatingsAndReviews>
       </div>
-    )
+    );
   }
 }
 
