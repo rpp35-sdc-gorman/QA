442484419b93c63c74037e4d078d5324b5417b55
Jonathan Tang <jtang312@gmail.com>
2022-05-13 00:14:55 -0400

    uncommented axios requests after updating product id

--- client/src/components/Questions_Answers/QAMain.jsx
+++ client/src/components/Questions_Answers/QAMain.jsx
@@ -8,35 +8,18 @@ class QAMain extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      questions: [{
-        "question_id": 37,
-        "question_body": "Why is this product cheaper here than other sites?",
-        "question_date": "2018-10-18T00:00:00.000Z",
-        "asker_name": "williamsmith",
-        "question_helpfulness": 4,
-        "reported": false,
-        "answers": {
-          68: {
-            "id": 68,
-            "body": "We are selling it here without any markup from the middleman!",
-            "date": "2018-08-18T00:00:00.000Z",
-            "answerer_name": "Seller",
-            "helpfulness": 4,
-            "photos": []
-          }
-        }
-      }]
+      questions: []
     };
   }
 
   componentDidMount() {
     // get initial QA list from server;
-    // axios.get(`/question_answer/questions/64620`)
-    //   .then(QAs => {
-    //     this.setState({
-    //       questions: QAs.data.results
-    //     })
-    //   })
+    axios.get(`/question_answer/questions/71697`)
+      .then(QAs => {
+        this.setState({
+          questions: QAs.data.results
+        })
+      })
   }
 
   render() {
--- client/src/components/Questions_Answers/SingleQA.jsx
+++ client/src/components/Questions_Answers/SingleQA.jsx
@@ -9,79 +9,20 @@ class SingleQA extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      allAnswers: [
-        {
-          "answer_id": 8,
-          "body": "What a great question!",
-          "date": "2018-01-04T00:00:00.000Z",
-          "answerer_name": "metslover",
-          "helpfulness": 8,
-          "photos": [],
-        },
-        {
-          "answer_id": 5,
-          "body": "Something pretty durable but I can't be sure",
-          "date": "2018-01-04T00:00:00.000Z",
-          "answerer_name": "metslover",
-          "helpfulness": 5,
-          "photos": [{
-              "id": 1,
-              "url": "urlplaceholder/answer_5_photo_number_1.jpg"
-            },
-            {
-              "id": 2,
-              "url": "urlplaceholder/answer_5_photo_number_2.jpg"
-            }
-          ]
-        },
-        {
-          "answer_id": 11,
-          "body": "Random answer",
-          "date": "2018-01-04T00:00:00.000Z",
-          "answerer_name": "Jon",
-          "helpfulness": 8,
-          "photos": [],
-        }
-      ],
-      dispAnswers: [
-        {
-          "answer_id": 8,
-          "body": "What a great question!",
-          "date": "2018-01-04T00:00:00.000Z",
-          "answerer_name": "metslover",
-          "helpfulness": 8,
-          "photos": [],
-        },
-        {
-          "answer_id": 5,
-          "body": "Something pretty durable but I can't be sure",
-          "date": "2018-01-04T00:00:00.000Z",
-          "answerer_name": "metslover",
-          "helpfulness": 5,
-          "photos": [{
-              "id": 1,
-              "url": "urlplaceholder/answer_5_photo_number_1.jpg"
-            },
-            {
-              "id": 2,
-              "url": "urlplaceholder/answer_5_photo_number_2.jpg"
-            }
-          ]
-        }
-      ],
+      allAnswers: [],
       showAnswers: false
     }
   }
 
   componentDidMount() {
     // get answers to question
-    // axios.get(`/question_answer/answers/${this.props.question.question_id}`)
-    //   .then(answers => {
-    //     this.setState({
-    //       allAnswers: answers.data.results.sort((a, b) => b.helpfulness - a.helpfulness),
-    //       dispAnswers: answers.data.results.slice(0, 2)
-    //     })
-    //   })
+    axios.get(`/question_answer/answers/${this.props.question.question_id}`)
+      .then(answers => {
+        this.setState({
+          allAnswers: answers.data.results.sort((a, b) => b.helpfulness - a.helpfulness),
+          dispAnswers: answers.data.results.slice(0, 2)
+        })
+      })
   }
 
   toggleAccordion(e) {
