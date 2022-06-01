161220cc2aebbf4a23c032eda0a84196c13e8039
Kevin On <idealistkevin@gmail.com>
2022-05-31 19:32:04 -0700

    Fixing some test

--- client/src/components/RIC_Component/Test/RIC.test.js
+++ client/src/components/RIC_Component/Test/RIC.test.js
@@ -2,34 +2,35 @@ import React from "react";
 import { unmountComponentAtNode } from "react-dom";
 import { createRoot } from 'react-dom/client';
 import { act } from "react-dom/test-utils";
+import exampleProducts from './ricTestData.js';
 import axios from 'axios';
 
 import RIC from '../RIC.jsx';
 
 global.IS_REACT_ACT_ENVIRONMENT = true
 jest.mock('axios');
+axios.get.mockResolvedValue(exampleProducts.products.data);
 
 let testId = 71697;
-let response =
 
 let container = null;
-// beforeEach(async () => {
-//   // setup a DOM element as a render target
-//   container = document.createElement("div");
-//   document.body.appendChild(container);
-//   await act(() => {
-//     createRoot(container).render(<RIC />)
-//   })
-// });
-// afterEach(() => {
-//   // cleanup on exiting
-//   unmountComponentAtNode(document);
-//   container.remove();
-//   container = null;
-// });
+beforeEach(async () => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+  await act(() => {
+    createRoot(container).render(<RIC />)
+  })
+});
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(document);
+  container.remove();
+  container = null;
+});
 
-// describe('RIC Parent Test', () =>{
-//   it('should load entire component', () => {
-//     expect(container.querySelector('div.RIC')).not.toBe(null);
-//   });
-// })
+describe('RIC Parent Test', () =>{
+  it('should load entire component', () => {
+    expect(container.querySelector('div.RIC')).not.toBe(null);
+  });
+})
