dc1bb96fd1a8bc4f1b688fc434f70a02439f1780
Kevin On <idealistkevin@gmail.com>
2022-06-08 21:41:48 -0700

    removed console.log

--- client/src/components/RIC_Component/YourOutfits.jsx
+++ client/src/components/RIC_Component/YourOutfits.jsx
@@ -21,7 +21,6 @@ class YourOutfits extends React.Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    console.log(prevState.yourOutfits, this.state.yourOutfits);
     if (this.props.added === false
       && (prevState.yourOutfits.length !== this.state.yourOutfits.length)
       && (this.state.removed === this.props.currentProduct.id)) {
