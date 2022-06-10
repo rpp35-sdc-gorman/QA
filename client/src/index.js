6fac1d1cd95c68cccf0532b4aff815feddd45971
Jon Personal <jtang312@gmail.com>
2022-06-10 12:10:44 -0400

    adding React.Suspense fallbacks

--- client/src/index.js
+++ client/src/index.js
@@ -10,6 +10,7 @@ const Banner = React.lazy(() => import('./components/Banner.jsx'));
 // import RIC from './components/RIC_Component/RIC.jsx';
 
 import ClickTracker from './components/common/ClickTracker.jsx';
+import Ratings from './components/ratingsAndReviews/ratings.jsx';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -24,10 +25,18 @@ class App extends React.Component {
     const QA = ClickTracker(QAMain, 'Question & Answer');
     return (
       <div>
-        <Banner />
-        <Coupler />
-        <QA />
-        <RatingsAndReviews />
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <Banner />
+        </React.Suspense>
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <Coupler />
+        </React.Suspense>
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <QA />
+        </React.Suspense>
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <RatingsAndReviews />
+        </React.Suspense>
       </div>
     );
   }
